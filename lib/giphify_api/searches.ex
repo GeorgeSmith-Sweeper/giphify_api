ae783279510f2184e19dc04b8dce791b8220ffa6
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-04-12 12:22:54 -0400

    add searches model

---
+++ lib/giphify_api/searches.ex
@@ -0,0 +1,16 @@
+defmodule GiphifyApi.Searches do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "searches" do
+    field :query, :string
+
+    timestamps()
+  end
+
+  def changeset(search, attrs) do
+    search
+    |> cast(attrs, [:query])
+    |> validate_required([:query])
+  end
+end
