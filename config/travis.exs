67638e89999383b22dfd3bbacbe562d01a75f8b7
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-04-25 09:54:22 -0400

    Setting up travis

---
+++ config/travis.exs
@@ -0,0 +1,22 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+
+config :giphify_api, :giphy_search, GiphifyApi.GiphySearch.MockClient
+
+config :giphify_api, GiphifyApiWeb.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :giphify_api, GiphifyApi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "8thlight",
+  password: "",
+  database: "giphify_api_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
